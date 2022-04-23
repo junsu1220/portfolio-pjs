3b03ba61cc414967fc596ede56321c4dea7ec447
junsu1220 <crab7157@gmail.com>
2022-04-23 11:31:33 +0900

    ClONES를 TOY로 변경

--- src/js/index.js
+++ src/js/index.js
@@ -12,7 +12,7 @@ const toHomeEl = document.querySelector("#home");
 const toAboutEl = document.querySelector("#about-me");
 const toSkillsEl = document.querySelector("#skills");
 const toProjectsEl = document.querySelector("#projects");
-const toClonesEl = document.querySelector("#clones");
+const toToyEl = document.querySelector("#toy");
 
 toTopEl.addEventListener("click", function () {
   gsap.to(window, 0.7, {
@@ -39,7 +39,7 @@ toProjectsEl.addEventListener("click", function () {
     scrollTo: 2280,
   });
 });
-toClonesEl.addEventListener("click", function () {
+toToyEl.addEventListener("click", function () {
   gsap.to(window, 0.7, {
     scrollTo: 3200,
   });
