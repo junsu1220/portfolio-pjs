2d4c47ffa900e7e584d92bc997c4189a1d56fcbf
junsu1220 <crab7157@gmail.com>
2022-04-23 10:24:21 +0900

    배포 package 수정

--- webpack.config.js
+++ webpack.config.js
@@ -40,10 +40,6 @@ module.exports = {
       }
     ],
   },
-  scripts: { 
-    dev: "webpack-dev-server",
-    build: "webpack --mode=production" 
-  },
   optimization: {
     minimizer: [
       new TerserPlugin(),
